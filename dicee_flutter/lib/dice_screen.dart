c8fe1e5b505f42fd12b67bf8b0388f7ea5235db8
Richard Rouzeau <richard@evigy.com>
2020-02-10 10:25:43 -0500

    Basic layout ready for dice

---
+++ dicee_flutter/lib/dice_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:dicee_flutter/dice_screen.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final Color mainColor = Colors.green[900];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: mainColor,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: mainColor,
+        ),
+        body: Dice(),
+      ),
+      color: mainColor,
+    );
+  }
+}
