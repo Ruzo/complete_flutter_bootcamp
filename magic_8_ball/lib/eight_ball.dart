31fabab2eb2569315aeb5e2390324b1fae04759d
Richard Rouzeau <richard@evigy.com>
2020-02-12 16:17:11 -0500

    Image on screen with tap to change answer

--- magic_8_ball/lib/eight_ball.dart
+++ magic_8_ball/lib/eight_ball.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class EightBall extends StatefulWidget {
   @override
@@ -6,8 +7,21 @@ class EightBall extends StatefulWidget {
 }
 
 class _EightBallState extends State<EightBall> {
+  int _answer = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: FlatButton(
+        child: Image.asset('images/ball$_answer.png'),
+        onPressed: () => getAnswer(),
+      ),
+    );
+  }
+
+  getAnswer() {
+    setState(() {
+      _answer = Random().nextInt(5) + 1;
+    });
   }
 }
