74020370c2140028c41d485500f8b639646f59ed
Richard Rouzeau <richard@evigy.com>
2020-02-17 10:14:54 -0500

    All done. Works on physical phone but not emulator

--- xylophone/lib/bars.dart
+++ xylophone/lib/bars.dart
@@ -12,31 +12,28 @@ class _BarsState extends State<Bars> {
       List(8).map((p) => p = AudioCache()).toList();
 
   @override
-  void initState() {
-    // _player.map((p) => p = AudioCache()).toList();
-    print(_player.toString());
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
       width: double.maxFinite,
       height: double.maxFinite,
-      child: Column(children: <Widget>[
-        for (var i = 1; i < 8; i++)
-          FlatButton(
-            key: Key('bar$i'),
-            padding: EdgeInsets.all(0),
-            onPressed: () => _player[i].play('note$i.wav'),
-            child: Container(
-              color: Color.fromRGBO(Random().nextInt(256),
-                  Random().nextInt(256), Random().nextInt(256), 1.0),
-              height: 100.0,
-            ),
-          )
-      ]),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          for (var i = 1; i < 8; i++)
+            Expanded(
+              child: FlatButton(
+                key: Key('bar$i'),
+                padding: EdgeInsets.all(0),
+                onPressed: () => _player[i].play('note$i.wav'),
+                child: Container(
+                  color: Color.fromRGBO(Random().nextInt(256),
+                      Random().nextInt(256), Random().nextInt(256), 1.0),
+                ),
+              ),
+            )
+        ],
+      ),
     );
   }
 }
