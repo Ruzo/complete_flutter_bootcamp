b190a35fb97423ed93f2030d7564680816482b76
Richard Rouzeau <richard@evigy.com>
2021-03-23 13:12:52 -0400

    Ready to end with Todoey app

---
+++ todoey/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/screens/app.dart';
+
+void main() {
+  runApp(App());
+}
---
+++ todoey/lib/screens/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Ready to Code!')),
+      ),
+    );
+  }
+}
